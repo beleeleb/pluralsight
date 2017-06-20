96f0f011a27afb1cb4c5c9647c024518ea78554c
gali zorea <galizorea@yans-macbook-pro.local>
2017-06-20 23:07:42 +0300

    Initial Commit

---
+++ pluralsight/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pluralsight
+//
+//  Created by gali zorea on 20/06/2017.
+//  Copyright © 2017 gali zorea. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
